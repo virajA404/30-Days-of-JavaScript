08f8e3cc69d89e285017136d8f0a10b1b2b5a742
Viraj Abeywardana <abeywardana21@gmail.com>
2023-08-28 00:10:25 +0530

    Exercise

--- Day 1/helloworld.js
+++ Day 1/helloworld.js
@@ -1,5 +1,47 @@
 console.log("Hello world!");
 
+//5
 console.log(typeof "Hello world!");
 console.log(typeof 5);
 console.log(typeof true);
+
+// Day 01 exercise
+
+//1 
+//this is the comment
+
+//2
+//Welcome to 30 days of JavaScript
+
+//3
+// comments can make code readable,
+// easy to reuse and informative
+
+//6
+let length 
+let numberOfBoxes 
+let distance 
+let foodType
+
+//7
+let foodName = 'Fast food'
+const gravity = 10
+let numberOfDays = 30
+const availability = true
+
+//8
+const firstName = 'Viraj'
+const lastName = 'Abeywardana'
+const maritalStatus = true
+let country = 'United States'
+let age = 25
+
+//9
+const firstName1 = 'Prabhavi', lastName1 = 'Subasinghe', maritalStatus1 = true, 
+ country1 = 'United States', age1 = 25
+
+myAge = '25'
+yourAge = '30'
+
+console.log("I am " + myAge +" years old.")
+console.log("You " + yourAge +" years old.")
---
+++ Day 1/variable.js
@@ -0,0 +1,8 @@
+console.log('This is the external js')
+
+//4
+let name = 'viraj'
+let answers = true
+let no 
+let empty = null
+console.log(empty)
