f64b2fdb5bd54df869d87eeb7005b64a63a7af3e
Viraj Abeywardana <abeywardana21@gmail.com>
2023-08-29 10:52:21 +0530

    data types

---
+++ Day 2/day2.js
@@ -0,0 +1,57 @@
+let word = "javascript"
+
+console.log(word);
+
+const PI = Math.PI
+console.log(PI);
+
+console.log(Math.round(PI));
+
+console.log(Math.round(9.81));
+
+console.log(Math.floor(9.81));
+
+console.log(Math.ceil(9.81));
+
+console.log(Math.min(0,-5, 8 ,2,1));
+
+console.log(Math.max(0,-5, 8 ,2,1));
+
+const num = Math.random();
+console.log(num);
+
+const num2 = Math.floor(Math.random()* 11);
+console.log(num2);
+
+console.log(Math.abs(-10.2))
+
+console.log(Math.sqrt(100))
+
+console.log(Math.pow(3,2))
+
+console.log(Math.E)
+
+console.log(Math.log(10))
+
+console.log(Math.LN2)
+
+console.log(Math.LN10)
+
+console.log(Math.sin(90))
+console.log(Math.cos(0))
+console.log(Math.tan(180))
+
+const paragraph = "My name is \\Asabeneh Yetayeh. I live in Finland, Helsinki.\nI am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, \tData Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a \'Python\' challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the \"30DaysOfJavaScript\" challenge and \
+I hope you are enjoying too."
+
+console.log(paragraph)
+
+let a = 4
+let b = 5
+console.log(`The sum of ${a} and ${b} is ${a +b}`)
+
